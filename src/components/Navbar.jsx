cf0990e57341c98ee79dcbf39a94d33482076d85
raihaneong <raihneong@gmail.com>
2023-02-25 16:38:07 +0700

    Checking on host environment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">rhntp store</a>
             </div>
             <div className="flex-none">
                 <div className="dropdown dropdown-end">
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -4,11 +4,11 @@
 export default function Store(){
     return(
         <div className="container mx-auto">
-            <h1>welcome</h1>
+            <h1 className=" m-8 p-8 text-5xl">welcome</h1>
             <h2>what do you wanna buy</h2>
             <p>take a look</p>
             <br />
-            <br />
+            <hr />
             <br />
             <a href="success">success | </a>
             <a href="cancel">cancel | </a>

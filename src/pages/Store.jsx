e7d702377192f6889a6fd39eb7f685b14a8f1a80
raihaneong <raihneong@gmail.com>
2023-02-25 20:15:30 +0700

    A Day!

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -10,7 +10,7 @@ export default function Store() {
     return (
         <>
             <h1>gg</h1>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-4 gap-8 p-32">
                 {productsArray.map((product) => (
                     <ProductsCard product={product} />
                 ))}

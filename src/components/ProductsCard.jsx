089569673e7be91fd770140532815134c34982b9
raihaneong <raihneong@gmail.com>
2023-02-26 06:55:05 +0700

    Adding dummy image

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -5,6 +5,7 @@ export default function ProductsCard({ name, price, description, id}) {
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl p-8">
+            <figure><img src="https://picsum.photos/400/200" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
                 <p className="card-subtitle">$ {price}</p>

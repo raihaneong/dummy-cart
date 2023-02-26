0b03e8c5d0a8779f5338af7cdcb5d04567e6544e
raihaneong <raihneong@gmail.com>
2023-02-26 11:56:55 +0700

    Solved!
    You should aware of every props you are destructuring at

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,8 +2,7 @@
 import { useContext } from "react"
 import {CartContext} from "../Context/CartContext"
 
-export default function ProductsCard(props) {
-    const products = props.products
+export default function ProductsCard( {name, price, description, id}) {
     const cart = useContext(CartContext)
     // const productQuantity = cart.getProductQuantity(id)
     console.log(cart.cartItems)
@@ -12,12 +11,11 @@ export default function ProductsCard(props) {
         <div className="card w-96 bg-base-100 shadow-xl p-8">
             <figure><img src="https://picsum.photos/400/200" alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{products.name}</h2>
-                <p className="card-subtitle">$ {products.price}</p>
-                <p>{products.description}</p>
+                <h2 className="card-title">{name}</h2>
+                <p className="card-subtitle">$ {price}</p>
+                <p>{description}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary" onClick={() => cart.addOne(products.id)} >Add to Cart</button>
-                    <p>{products.id}</p>
+                    <button className="btn btn-primary" onClick={() => cart.addOne(id)} >Add to Cart</button>
                 </div>
             </div>
         </div>

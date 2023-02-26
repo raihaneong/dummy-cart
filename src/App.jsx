02087db22ab789ff40744f92271063bf5d1f3ade
raihaneong <raihneong@gmail.com>
2023-02-26 10:46:13 +0700

    Wrapping cart context

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import NotFound from "./pages/NotFound"
 import Cancel from "./pages/Cancel"
 import Success from "./pages/Success"
 
+import CartProvider from './Context/CartContext'
+
 
 
 export default function App() {
     return (
-        <div >
-            <NavbarComponent></NavbarComponent>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={ <Store />} />
-                    <Route path="/*" element={ <NotFound />} />
-                    <Route path="/Cancel" element={ <Cancel />} />
-                    <Route path="/Success" element={ <Success/>} />
-                </Routes>
-            </BrowserRouter>
-
-        </ div>
+        <CartProvider>
+            <div >
+                <NavbarComponent></NavbarComponent>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Store />} />
+                        <Route path="/*" element={<NotFound />} />
+                        <Route path="/Cancel" element={<Cancel />} />
+                        <Route path="/Success" element={<Success />} />
+                    </Routes>
+                </BrowserRouter>
+            </ div>
+        </CartProvider>
     )
 }

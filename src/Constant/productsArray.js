14e50668ca21029680eac070f9419718a7e679c4
raihaneong <raihneong@gmail.com>
2023-02-26 22:35:10 +0700

    Changing the prices

--- src/Constant/productsArray.js
+++ src/Constant/productsArray.js
@@ -3,37 +3,37 @@ const productsArray = [
     {
         id: 1,
         name: "Tea",
-        price: 3,
+        price: 3.49,
         description: "A cup of tea that makes you feel good",
     },
     {
         id: 2,
         name: "Coffee",
-        price: 5,
+        price: 5.99,
         description: "Enjoy a cup of coffee to start your day",
     },
     {
         id: 3,
         name: "Sunglasses",
-        price: 10,
+        price: 10.99,
         description: "A pair of sunglasses to protect your eyes",
     },
     {
         id: 4,
         name: "Laptop",
-        price: 1000,
+        price: 1499.99,
         description: "a laptop to work from home",
     },
     {
         id: 5,
         name: "Camera",
-        price: 30,
+        price: 30.49,
         description: "Take pictures with this camera",
     },
     {
         id: 6,
         name: "Bag",
-        price: 15,
+        price: 14.99,
         description: "Bag to carry your stuff",
     }, 
 

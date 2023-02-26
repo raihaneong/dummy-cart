d4e2bb49371102eb8f3aed3be8b6d3521a08af0b
raihaneong <raihneong@gmail.com>
2023-02-26 07:28:52 +0700

    Modify close button in modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
             <input type="checkbox" id="my-modal-3" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box relative">
-                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2 bg-red-600 hover:bg-red-900">✕</label>
                     <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
                     <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                 </div>

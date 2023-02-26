ffbbcc8fc8f19f8e2c9153867cfbd5bf09c8520e
raihaneong <raihneong@gmail.com>
2023-02-26 23:12:00 +0700

    Changing the id into stripe-made id

--- src/Constant/productsArray.js
+++ src/Constant/productsArray.js
@@ -1,37 +1,37 @@
 
 const productsArray = [
     {
-        id: 1,
+        id: 'price_1MfmARK6L9E8BuRsT76In9Qi',
         name: "Tea",
         price: 3.49,
         description: "A cup of tea that makes you feel good",
     },
     {
-        id: 2,
+        id: 'price_1MfmBYK6L9E8BuRsSoMl2f0e',
         name: "Coffee",
         price: 5.99,
         description: "Enjoy a cup of coffee to start your day",
     },
     {
-        id: 3,
+        id: 'price_1MfmC9K6L9E8BuRsq5ZDjuKF',
         name: "Sunglasses",
         price: 10.99,
         description: "A pair of sunglasses to protect your eyes",
     },
     {
-        id: 4,
+        id: 'price_1MfmD8K6L9E8BuRsRyaGJ2be',
         name: "Laptop",
         price: 1499.99,
         description: "a laptop to work from home",
     },
     {
-        id: 5,
+        id: 'price_1MfmDoK6L9E8BuRszIcubN8P',
         name: "Camera",
         price: 30.49,
         description: "Take pictures with this camera",
     },
     {
-        id: 6,
+        id: 'price_1MfmESK6L9E8BuRsN60BLMe1',
         name: "Bag",
         price: 14.99,
         description: "Bag to carry your stuff",

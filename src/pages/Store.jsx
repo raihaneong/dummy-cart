df72c964facb6391a41ace1bfea58f1b5dc4cf0d
raihaneong <raihneong@gmail.com>
2023-02-27 07:28:43 +0700

    Designing cards

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -11,7 +11,7 @@ export default function Store() {
 
     return (
         <>
-            <div className="grid grid-cols-5 gap-8 p-32">
+            <div className="grid grid-cols-1 p-8 m-8 md:grid-cols-2 lg:grid-cols-5">
                 {ProductsCardElement}
             </div>
 
